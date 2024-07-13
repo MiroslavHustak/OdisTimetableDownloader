b8be4e2342a2f7d08a1dd1e50364ed81244ceed7
Miroslav Husťák <97535187+MiroslavHustak@users.noreply.github.com>
2024-07-14 00:18:15 +0200

    13-07-2024

--- OdisTimetableDownloader/Helpers/ProgressBarFSharp.fs
+++ OdisTimetableDownloader/Helpers/ProgressBarFSharp.fs
@@ -15,59 +15,36 @@ module ProgressBarFSharp =
     
     let inline private updateProgressBar (currentProgress : int) (totalProgress : int) : unit =
 
-        let bytes = //437 je tzv. Extended ASCII            
-            try
-                Ok <| System.Text.Encoding.GetEncoding(437).GetBytes("█")            
-            with
-            | ex -> Error <| string ex.Message
-                                       
-            |> function
-                | Ok value  -> value  
-                | Error err -> [||]  //nestoji to za to
+        try
+            //437 je tzv. Extended ASCII   
+            let bytes = System.Text.Encoding.GetEncoding(437).GetBytes("█")    
                    
-        let output =          
-            try
-                Ok <| System.Text.Encoding.GetEncoding(852).GetChars(bytes)
-            with
-            | ex -> Error <| string ex.Message
-                                       
-            |> function
-                | Ok value  -> value  
-                | Error err -> [||]  //nestoji to za to
-        
-        let progressBar = 
+            let output = System.Text.Encoding.GetEncoding(852).GetChars(bytes)
+                      
+            let progressBar = 
 
-            let barWidth = 50 //nastavit delku dle potreby            
-            let percentComplete = (currentProgress * 101) / (totalProgress + 1) //101 proto, ze pri deleni 100 to po zaokrouhleni dalo jen 99%                    
-            let barFill = (currentProgress * barWidth) / totalProgress 
+                let barWidth = 50 //nastavit delku dle potreby            
+                let percentComplete = (currentProgress * 101) / (totalProgress + 1) //101 proto, ze pri deleni 100 to po zaokrouhleni dalo jen 99%                    
+                let barFill = (currentProgress * barWidth) / totalProgress 
                
-            let characterToFill = string (Array.item 0 output) //moze byt baj aji "#"
+                let characterToFill = string (Array.item 0 output) //moze byt baj aji "#"
             
-            let bar = 
-                try                   
-                    Ok <| String.replicate barFill characterToFill
-                with
-                | ex -> Error <| string ex.Message
-                                           
-                |> function
-                    | Ok value  -> value  
-                    | Error err -> String.Empty //nestoji to za to
-                               
-            let remaining = 
-                try
-                    Ok <| String.replicate (barWidth - (barFill + 1)) "*"
-                with
-                | ex -> Error <| string ex.Message
-                                           
-                |> function
-                    | Ok value  -> value  
-                    | Error err -> String.Empty //nestoji to za to 
-              
-            sprintf "<%s%s> %d%%" bar remaining percentComplete 
+                let bar = string <| String.replicate barFill characterToFill //Option.ofNullEmpty tady nestoji za tu namahu
+                                                 
+                let remaining = string <| String.replicate (barWidth - (barFill + 1)) "*" //Option.ofNullEmpty tady nestoji za tu namahu
+                                  
+                sprintf "<%s%s> %d%%" bar remaining percentComplete 
 
-        match (=) currentProgress totalProgress with
-        | true  -> msgParam8 progressBar
-        | false -> msgParam9 progressBar
+            match (=) currentProgress totalProgress with
+            | true  -> Ok <| msgParam8 progressBar
+            | false -> Ok <| msgParam9 progressBar
+
+        with
+        | ex -> Error <| string ex.Message
+                               
+        |> function
+            | Ok value -> value  
+            | Error _  -> () //nestoji to za to neco s tim delat   
                                  
     let internal progressBarContinuous (currentProgress : int) (totalProgress : int) : unit =
 
