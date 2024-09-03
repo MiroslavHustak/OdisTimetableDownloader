3b4603859cf042f01ecad6b417ad123e56eb2cd3
Miroslav Husťák <97535187+MiroslavHustak@users.noreply.github.com>
2024-09-03 17:05:14 +0200

    03-09-2024

--- OdisTimetableDownloader/SubmainFunctions/DPO-Submain.fs
+++ OdisTimetableDownloader/SubmainFunctions/DPO-Submain.fs
@@ -63,12 +63,15 @@ module DPO_Submain =
                           )  
                       |> Seq.filter
                           (fun (_ , item2) ->
-                                            item2.Contains @"/jr/" && item2.Contains ".pdf" && not (item2.Contains "AE-eng.pdf") 
+                                            item2.Contains @"/jr/"
+                                            && 
+                                            item2.Contains ".pdf" 
+                                            && 
+                                            not (item2.Contains "AE-en")
                           )
                       |> Seq.map 
                           (fun (_ , item2) ->  
                                             let linkToPdf = sprintf"%s%s" pathDpoWeb item2  //https://www.dpo.cz // /jr/2023-04-01/024.pdf 
-
                                             let adaptedLineName =
                                                 let s (item2: string) = item2.Replace(@"/jr/", String.Empty).Replace(@"/", "?").Replace(".pdf", String.Empty) 
                                                 let rec x s =                                                                            
