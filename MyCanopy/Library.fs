2ba7f5357f468fc02279887d39ad5c2d62c20eee
Miroslav Husťák <97535187+MiroslavHustak@users.noreply.github.com>
2024-10-28 02:30:36 +0100

    28-10-2024

--- MyCanopy/Library.fs
+++ MyCanopy/Library.fs
@@ -17,36 +17,37 @@ module MyCanopy =
     
             canopy.configuration.compareTimeout <- 100.0 
     
-            let linksShown () = (canopy.classic.elements ".Card_wrapper__ZQ5Fp").Length >= 1 
+            //In CSS selectors, the . before a word or identifier specifies a class.    
+            //<li class="Card_wrapper__ZQ5Fp">
+            let linksShown () = (canopy.classic.elements ".Card_wrapper__ZQ5Fp").Length >= 1 // //".Card_wrapper__ZQ5Fp" //.Card_actions__HhB_f
             
             let urls = 
                 [
                     "https://www.kodis.cz/lines/city?tab=MHD+Ostrava"
                     "https://www.kodis.cz/lines/region?tab=75" 
+                    "https://www.kodis.cz/lines/city?tab=MHD+Opava"
                     "https://www.kodis.cz/lines/region?tab=232-293"
+                    "https://www.kodis.cz/lines/city?tab=MHD+Frýdek-Místek"
                     "https://www.kodis.cz/lines/region?tab=331-392"
+                    "https://www.kodis.cz/lines/city?tab=MHD+Havířov"
                     "https://www.kodis.cz/lines/region?tab=440-465"
+                    "https://www.kodis.cz/lines/city?tab=MHD+Karviná"
+                    "https://www.kodis.cz/lines/city?tab=MHD+Orlová"
                     "https://www.kodis.cz/lines/region?tab=531-583"
+                    "https://www.kodis.cz/lines/city?tab=MHD+Nový+Jičín"
+                    "https://www.kodis.cz/lines/city?tab=MHD+Studénka"
                     "https://www.kodis.cz/lines/region?tab=613-699"
+                    "https://www.kodis.cz/lines/city?tab=MHD+Třinec"
+                    "https://www.kodis.cz/lines/city?tab=MHD+Český+Těšín"
                     "https://www.kodis.cz/lines/region?tab=731-788"
+                    "https://www.kodis.cz/lines/city?tab=MHD+Krnov"
+                    "https://www.kodis.cz/lines/city?tab=MHD+Bruntál"
                     "https://www.kodis.cz/lines/region?tab=811-885"
                     "https://www.kodis.cz/lines/region?tab=901-990"
-                    "https://www.kodis.cz/lines/region?tab=NAD"
-                    "https://www.kodis.cz/lines/city?tab=MHD+Bruntál"
-                    "https://www.kodis.cz/lines/city?tab=MHD+Český+Těšín"
-                    "https://www.kodis.cz/lines/city?tab=MHD+Frýdek-Místek"
-                    "https://www.kodis.cz/lines/city?tab=MHD+Havířov"
-                    "https://www.kodis.cz/lines/city?tab=MHD+Karviná"
-                    "https://www.kodis.cz/lines/city?tab=MHD+Krnov"
-                    "https://www.kodis.cz/lines/city?tab=MHD+Nový+Jičín"
-                    "https://www.kodis.cz/lines/city?tab=MHD+Opava"
-                    "https://www.kodis.cz/lines/city?tab=MHD+Orlová"
-                    "https://www.kodis.cz/lines/city?tab=MHD+Ostrava"
-                    "https://www.kodis.cz/lines/city?tab=MHD+Studénka"
-                    "https://www.kodis.cz/lines/city?tab=MHD+Třinec"
-                    "https://www.kodis.cz/lines/city?tab=NAD+MHD"
                     "https://www.kodis.cz/lines/train?tab=S1-S34"
                     "https://www.kodis.cz/lines/train?tab=R8-R62"
+                    "https://www.kodis.cz/lines/city?tab=NAD+MHD"
+                    "https://www.kodis.cz/lines/region?tab=NAD"   
                 ]
 
             let scrapeUrl (url: string) =
@@ -58,7 +59,7 @@ module MyCanopy =
                     Thread.Sleep 10000                     
                     canopy.classic.waitFor linksShown 
                     
-                    canopy.classic.elements "a" 
+                    canopy.classic.elements "a[title='Aktuální jízdní řád']" 
                     |> Seq.map 
                         (fun item -> 
                                    let href = string <| item.GetAttribute("href")
